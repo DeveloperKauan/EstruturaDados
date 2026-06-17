d05e2985d2e60208e240997b2cff7143738ad262
Kauan <166316814+DeveloperKauan@users.noreply.github.com>
2026-06-16 22:32:46 -0300

    docs: cria estrutura inicial e documenta a classe da Lista Encadeada

--- src/lista_encadeada.py
+++ src/lista_encadeada.py
@@ -1,1 +1,9 @@
-
+#Nome da Classe Principal da Lista Encadeada
+"""
+class ListaDuplamenteEncadeada:
+    def inserir_ordenado(self, ordem): ...  # insere mantendo a ordem correta
+    def remover(self, id):             ...  # remove nó pelo ID
+    def topo(self):                    ...  # retorna o primeiro nó sem remover
+    def is_empty(self):                ...  # retorna True se vazia
+    def exibir(self):                  ...  # printa a lista pra debug
+"""
