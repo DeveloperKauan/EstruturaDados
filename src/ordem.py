ef68f40692ac863cb9b27304855c72d12e036a25
Kauan <166316814+DeveloperKauan@users.noreply.github.com>
2026-06-16 22:09:18 -0300

    Add OrdemNode class with constructor definition

--- src/ordem.py
+++ src/ordem.py
@@ -1,1 +1,8 @@
-
+# Nome da classe principal
+""" Classe OrdemNode:
+```python
+class OrdemNode:
+    def __init__(self, id, tipo, preco, quantidade, timestamp):
+        # tipo: 'C' para Compra, 'V' para Venda
+```
+"""
