76ec2ef915464869b80da9e317981c0bc65d8bd4
maria olívia. <oliviamecasiqueira@gmail.com>
2026-06-17 19:59:33 -0300

    Implementa Classe OrdemNode

--- src/ordem.py
+++ src/ordem.py
@@ -6,3 +6,18 @@ class OrdemNode:
         # tipo: 'C' para Compra, 'V' para Venda
 ```
 """
+
+# importações:
+from datetime import datetime
+
+class OrdemNode:
+
+  # as ordens de negociação que serão inseridas
+
+  def __init__(self, id:int, tipo:str, preco:float, quantidade:int):
+
+    self.id = id
+    self.tipo = tipo  # 'C' compra ou 'V' venda
+    self.preco = preco
+    self.quantidade = quantidade
+    self.timestamp = datetime.now()
